88894cda9cd3ed5a344c3365ba8f17b46fcd186b
Shreya Balaji <sbala025@wch136-21.cs.ucr.edu>
2021-10-03 10:38:53 -0700

    completed part 1

--- Lab2_introToAVR/turnin/sbala025_lab2_part1.c
+++ Lab2_introToAVR/turnin/sbala025_lab2_part1.c
@@ -19,13 +19,17 @@ int main(void) {
     DDRB = 0xFF; PORTB = 0x00; // Configure port B's 8 pins as outputs
     // Initialize output on PORTB to 0x00
     unsigned char input = PINA; // Temp hold value of A
+    unsigned char input2 = PINA; // Temp hold value of A
     //unsigned char output = 0x00; // Temp hold value of B
 				
     /* Insert your solution below */
     while(1){
-         if(((input & 0x01)  == 0x01) && ((input & 0x02)  == 0x00)) {
+         if(((input & 0x01)  == 0x01) && ((input2 & 0x02)  == 0x00)) {
 	      PORTB = 0x01;					
          }
+	else{
+	      PORTB = 0x00;
+	}
     }
     return 1;
 }
