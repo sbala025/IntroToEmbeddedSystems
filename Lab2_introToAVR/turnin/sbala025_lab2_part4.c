1210ecb2d9c1fc30cb34e29a72c3a52f5fb78d68
Shreya Balaji <sbala025@wch136-21.cs.ucr.edu>
2021-10-04 09:46:46 -0700

    completed part 4

--- Lab2_introToAVR/turnin/sbala025_lab2_part4.c
+++ Lab2_introToAVR/turnin/sbala025_lab2_part4.c
@@ -48,10 +48,10 @@ int main(void) {
 
 	//Check if ride is balanced --> D1
 	if(inputA > inputB){
-		if((inputA-inputB) > 0x8C){balanced = 0x02;}
+		if((inputA-inputB) > 0x50){balanced = 0x02;}
 	}
 	if(inputB > inputA){
-                if((inputB-inputA) > 0x8C){balanced = 0x02;}
+                if((inputB-inputA) > 0x50){balanced = 0x02;}
         }
 
 	//Convert weight to be only six bits 0x0000 --> 0x00XX
