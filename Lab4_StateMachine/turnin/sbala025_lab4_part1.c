ac4fcf819380d6dce1daee526f436d5ba1ff1c71
Shreya Balaji <sbala025@wch136-21.cs.ucr.edu>
2021-10-06 10:13:51 -0700

    completed part 1 .c

--- Lab4_StateMachine/turnin/sbala025_lab4_part1.c
+++ Lab4_StateMachine/turnin/sbala025_lab4_part1.c
@@ -12,12 +12,70 @@
 #include "simAVRHeader.h"
 #endif
 
+//ENUM INITIALIZATION
+enum States = {Start, OFF_R, OFF_P, ON_R, ON_P} state;
+
+//TICK FUNCTION
+void Tick(){
+	unsigned char input = PINA & 0x01;
+	switch(state){
+		case Start:
+			state = OFF_R;
+			break;
+		 case OFF_R:
+                        if(input == 1){
+                                state = ON_P;
+                                PORTB = 0x02;
+                        }
+                        else{   
+                                state = OFF_R;
+                                PORTB = 0x01;
+                        }
+                        break;
+		case ON_P:
+			if(input == 1){
+				state = ON_P;
+				PORTB = 0x02;
+			}
+			else{
+				state = ON_R;
+				PORTB = 0x02;
+			}
+			break;
+		case ON_R:
+                        if(input == 1){
+                                state = OFF_P;
+                                PORTB = 0x01;
+                        }
+                        else{
+                                state = ON_R;
+                                PORTB = 0x02;
+                        }
+                        break;
+		case OFF_P:
+                        if(input == 1){
+                                state = OFF_P;
+                                PORTB = 0x01;
+                        }
+                        else{
+                                state = OFF_R;
+                                PORTB = 0x01;
+                        }
+                        break;
+		default:
+			case = OFF_R;
+			break
+	}
+}
+
+//MAIN FUNCTION
 int main(void) {
-    /* Insert DDR and PORT initializations */
-
-    /* Insert your solution below */
-    while (1) {
-
-    }
-    return 1;
+	/* Insert DDR and PORT initializations */
+	DDRA = 0x00; PORTA = 0xFF;
+    	DDRB = 0xFF; PORTB = 0x00;
+	/* Insert your solution below */
+	while (1) {
+		Tick();
+	}
+	return 1;
 }
