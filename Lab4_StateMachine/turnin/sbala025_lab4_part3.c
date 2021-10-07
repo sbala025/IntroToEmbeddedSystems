549716766225e5d2ff11fb7d20b7fc98a28fc2f8
Shreya Balaji <sbala025@wch136-21.cs.ucr.edu>
2021-10-07 09:51:28 -0700

    initial commit part 3

---
+++ Lab4_StateMachine/turnin/sbala025_lab4_part3.c
@@ -0,0 +1,28 @@
+/*	Author: sbala025
+ *  Partner(s) Name: 
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+enum States {Start}state;
+
+void Tick(){
+
+}
+int main(void) {
+	/* Insert DDR and PORT initializations */
+	DDRA = 0x00; PORTA = 0xFF;
+	DDRB = 0xFF; PORTB = 0x00;
+	/* Insert your solution below */
+	while (1) {
+		Tick();
+	}
+	return 1;
+}
