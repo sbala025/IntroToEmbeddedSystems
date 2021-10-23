89b966f2b84d5040c3c8350dbcfbf36fa0f130ff
Shreya Balaji <sbala025@wch136-21.cs.ucr.edu>
2021-10-23 09:51:17 -0700

    completed part 2.c

--- Lab6_TimerSynchSMs/turnin/sbala025_lab6_part2.c
+++ Lab6_TimerSynchSMs/turnin/sbala025_lab6_part2.c
@@ -12,12 +12,122 @@
 #include "simAVRHeader.h"
 #endif
 
-int main(void) {
-    /* Insert DDR and PORT initializations */
+enum States {Start, OFF, LED, PAUSE, RIGHT, LEFT} state;
+volatile unsigned char TimerFlag = 0;
+/* Internal variables for mapping AVR’s ISR to our cleaner Timer ISR model */
+unsigned long _avr_timer_M = 1;
+unsigned long _avr_timer_cntcurr = 0;
+unsigned char switchDirection = 0x00;
 
-    /* Insert your solution below */
-    while (1) {
-
-    }
-    return 1;
+void TimerOn() {
+	TCCR1B = 0x0B;
+	OCR1A = 125;
+	TIMSK1 = 0x02;
+	TCNT1 = 0;
+	_avr_timer_cntcurr = _avr_timer_M;
+	SREG |= 0x80;
 }
+
+void TimerOff() {
+	TCCR1B = 0x00;
+}
+
+void TimerISR() { 
+	TimerFlag = 1;
+}
+
+ISR(TIMER1_COMPA_vect) {
+	_avr_timer_cntcurr--;
+	if (_avr_timer_cntcurr == 0) {
+		TimerISR();
+		_avr_timer_cntcurr = _avr_timer_M;
+	}
+}
+
+void TimerSet (unsigned long M) {
+	_avr_timer_M = M;
+	_avr_timer_cntcurr = _avr_timer_M;
+}
+
+void Tick() {
+	unsigned char input = ~PINA & 0X01;
+	switch(state) {
+		case Start:	
+			state = Init; 
+			break;
+		case OFF:	
+			state = LED; 
+			break;
+		case LED:	
+			if (input == 0x01) {
+				state = PAUSE;
+			}else {
+				state = LED;
+			}break;
+		case PAUSE:	
+			if (input == 0x01) {
+				state = Pause;
+			}else {
+				state = LEFT;
+			}break;
+		case LEFT:	
+			if (input == 0x01) {
+				state = RIGHT;
+			}else {
+				state = LEFT;
+			}break;
+		case RIGHT:	
+			if (input == 0x01) {
+				state = RIGHT;
+			}else {
+				state = Init;
+			}break;
+		default:	
+			state = Start; 
+			break;
+	}
+	switch (state) {
+		case Start:     
+			break;
+               	case OFF:      
+			PORTB = 0x01; 
+			break;
+		case NextLed:
+			if (switchDirection == 0x00) {
+				if (PORTB == 0x04) {
+					PORTB = PORTB >> 1;
+					switchDirection = 0x01;
+				}else {
+					PORTB = PORTB << 1;
+				}
+			}
+			else {
+				if (PORTB == 0x01) {
+                                        PORTB = PORTB << 1;
+                                       	switchDirection = 0x00;
+                               	} else {
+                                        PORTB = PORTB >> 1;
+                              	}
+			} break;
+		case PAUSE: 	
+			break;
+		case LEFT:	
+			break;
+		case RIGHT: 	
+			break;
+                default:        
+			break;
+	}
+}
+
+void main(void) {
+    DDRB = 0xFF; PORTB = 0x00;
+    TimerSet(300);
+    TimerOn();
+    while (1) {
+	Tick();
+	while(!TimerFlag) {};
+	TimerFlag = 0;
+    }
+}
+
