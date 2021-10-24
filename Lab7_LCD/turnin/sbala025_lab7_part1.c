53cc5978d392fc12c3843c3bf1ae04519c3916cf
Shreya Balaji <sbala025@wch136-21.cs.ucr.edu>
2021-10-24 15:47:20 -0700

    part 1 complete

--- Lab7_LCD/turnin/sbala025_lab7_part1.c
+++ Lab7_LCD/turnin/sbala025_lab7_part1.c
@@ -8,6 +8,7 @@
  *	code, is my own original work.
  */
 #include <avr/io.h>
+#include <avr/interrupt.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #endif
@@ -17,7 +18,8 @@ volatile unsigned char TimerFlag = 0;
 /* Internal variables for mapping AVR’s ISR to our cleaner Timer ISR model */
 unsigned long _avr_timer_M = 1;
 unsigned long _avr_timer_cntcurr = 0;
-
+unsigned char count = 0;
+unsigned char output = 0;
 void TimerOn() {
 TCCR1B = 0x0B;
 OCR1A = 125;
@@ -49,8 +51,7 @@ _avr_timer_cntcurr = _avr_timer_M;
 }
 
 void Tick(){
-	unsigned char input = PINA & 0x03;
-	unsigned char output = PORTC;
+	unsigned char input = ~PINA & 0x03;
 	switch (state){
 		case Start:
 			state = Actual_start;
@@ -71,21 +72,27 @@ void Tick(){
 			break;
 		case plus:
 			state = plus_down;
+			count = 0;
 			break;
 		case plus_down:
-			if(input == 0x01){state = plus_down;}
+			count++;
+			if(count == 9){state = plus; count = 0;}
+			else if(input == 0x01){state = plus_down;}
 			else if(input == 0x02){state = minus;}
 			else if (input == 0x03){state = reset;}
 			else {state = initialize;}
                         		break;
 		case minus_down:
-                        	if(input == 0x02){state = minus_down;}
-                        	else if(input == 0x01){state = plus;}
-                        	else if (input == 0x03){state = reset;}
-                        	else {state = initialize;}
-                        	break;
+			count++;
+			if(count == 9){state = minus; count = 0;}
+                        else if(input == 0x02){state = minus_down;}
+                        else if(input == 0x01){state = plus;}
+                        else if (input == 0x03){state = reset;}
+                        else {state = initialize;}
+                        break;
 		case minus:
 			state = minus_down;
+			count = 0;
 			break;
 		default:
 			state = initialize;
@@ -106,33 +113,24 @@ void Tick(){
                         break;
 		case plus:
 			if(output < 9){output = output + 1;}
+			//count = 0;
 			break;
 		case minus:
 			if(output > 0){output = output - 1;}
+			//count = 0;
 			break;
 		default:
 			output = 7;
                         break;
 	}
-	PORTC = output;
-	if (PORTC == 0x00) {LCD_DisplayString(1, 0 + ‘0’);}
-	else if (PORTC == 0x01) {LCD_DisplayString(1, 1 + ‘0’);}
-	else if (PORTC == 0x02) {LCD_DisplayString(1, 2 + ‘0’); }
-	else if (PORTC == 0x03) {LCD_DisplayString(1, 3 + ‘0’);}
-	else if (PORTC == 0x04) {LCD_DisplayString(1, 4 + ‘0’); }
-	else if (PORTC == 0x05) {LCD_DisplayString(1, 5 + ‘0’);}
-	else if (PORTC == 0x06) {LCD_DisplayString(1, 6 + ‘0’);}
-	else if (PORTC == 0x07) {LCD_DisplayString(1, 7 + ‘0’); }
-	else if (PORTC == 0x08) {LCD_DisplayString(1, 8 + ‘0’);}
-	else if (PORTC == 0x09) {LCD_DisplayString(1, 9 + ‘0’);}
-
+//	count++;
+	LCD_DisplayString(1, output + ‘0’);
 }
 /*MAIN FUNCTION*/
 int main(void) {
 	DDRA = 0x00; PORTA = 0xFF;
-    	DDRC = 0xFF; PORTC = 0x00;
-    	DDRD = 0xFF; PORTD = 0x00;
-	LCD_init();
+	DDRC = 0xFF; PORTC = 0x00;
+	DDRD = 0xFF; PORTD = 0x00;
 	TimerSet(100);
     	TimerOn();
     	while (1) {
@@ -141,3 +139,4 @@ int main(void) {
 		TimerFlag = 0;
    	 }
 }
+
