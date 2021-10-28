d807c662da3567eeebaa753d4f2f4daf94ce6874
Shreya Balaji <sbala025@wch136-21.cs.ucr.edu>
2021-10-28 16:00:43 -0700

    completed part 2

---
+++ Lab8_ADC/turnin/sbala025_lab8_part2.c
@@ -0,0 +1,38 @@
+/*	Author: sbala025
+ *  Partner(s) Name: 
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+unsigned short my_short = 0x0000;
+unsigned char my_char_upper = 0x00;
+unsigned char my_char_lower = 0x00;
+
+void ADC_init() {
+    ADCSRA |= (1<<ADEN) | (1<<ADSC) | (1<<ADATE);
+}
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+    DDRB = 0XFF; PORTB = 0X00;
+    DDRD = 0XFF; PORTD = 0X00;
+    /* Insert your solution below */
+    ADC_init();
+    while (1) {
+    my_short = ADC;
+    my_char_lower = (char)my_short;
+    my_char_upper = (char)my_short>>8;
+    PORTB = my_char_lower;
+    PORTD = my_char_upper;
+    }
+    return 1;
+}
